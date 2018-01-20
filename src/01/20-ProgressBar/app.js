62e5ebac5624eb510039962fc2faae39f3d9a86b
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-01-20 07:12:17 +0100

    Add 20-ProgressBar

---
+++ src/01/20-ProgressBar/app.js
@@ -0,0 +1,13 @@
+let i = 10;
+
+const bar = document.getElementById('message');
+const id = setInterval(() => {
+    if (i == 100) {
+        bar.innerText = 'Progress Complete';
+        clearInterval(id);
+    } else {
+        bar.innerText =  i + ' %';
+        bar.setAttribute('style', 'width:' + i + '%;');
+        i += Math.floor(Math.random() * 3);
+    }
+}, 1000)
