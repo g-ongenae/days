a5d8b73df60d80fdad45ead920f7fb2ee00ead1f
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-01-23 06:55:08 +0100

    Add 23-ReverseWord

---
+++ src/01/23-ReverseWord/app.js
@@ -0,0 +1,20 @@
+const reverseWordInString = (string) => {
+    return string.split(' ')
+        .map((word) => {
+            // Reverse word
+            word = word.split('')
+                .reverse()
+                .join('');
+            // Put the word in title case
+            return word.charAt(0).toUpperCase() +
+                word.substring(1).toLowerCase();
+        })
+        .join(' ');
+};
+
+const message = document.getElementById('message');
+const forms = document.forms[0];
+const reverse = document.getElementById('reverse');
+reverse.addEventListener('click', () => {
+    message.innerText = reverseWordInString(forms['text'].value);
+});
