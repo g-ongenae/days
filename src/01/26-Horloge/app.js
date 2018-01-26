f775a0589ae72203bd51649ea0b082de1397a201
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-01-26 08:01:17 +0100

    Add 26-Horloge

---
+++ src/01/26-Horloge/app.js
@@ -0,0 +1,23 @@
+const timer = () => {
+    const time = new Date();
+
+    let minutes = time.getMinutes();
+    minutes = minutes == 0 ? '' : minutes + ' min'; 
+
+    let hours = time.getHours();
+    if (hours == 0 || hours == 12) {
+        hours = hours == 0 ? 'minuit' : 'midi' ;
+        hours = minutes = '' ? hours : hours + ' et ';
+    } else {
+        hours = minutes == '' ? hours + ' heures' : hours + ' h ';
+    }
+
+    document
+        .getElementById('message')
+        .innerText = 'Il est ' + hours + minutes + '.';
+};
+
+// Initialize timer.
+timer();
+
+setInterval(timer, 30000);
