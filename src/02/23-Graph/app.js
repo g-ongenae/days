809e60b8c382cd9c81c83ffab8e04f8177232a84
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-02-23 23:53:02 +0100

    Add 23 Graph

---
+++ src/02/23-Graph/app.js
@@ -0,0 +1,19 @@
+const main  = document.getElementsByTagName('main')[0];
+
+const randomDiv = () => {
+    const divs = main.childNodes;
+
+    for (let i = 0; i < divs.length; i++) {
+        if (divs[i].nodeName === '#text') continue;
+
+        divs[i].setAttribute('style', `
+            background-color: #${(Math.random() * 0xFFFFFF<<0).toString(16)};
+            height: ${Math.ceil(Math.random() * 350)}px;
+        `);
+    }
+};
+
+// Init
+randomDiv();
+
+setInterval(randomDiv, 1000);
