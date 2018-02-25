52e1f0c682c011ad96562532d008c2f5ff421440
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-02-25 23:07:02 +0100

    Add 25 Snow

---
+++ src/02/25-Snow/app.js
@@ -0,0 +1,14 @@
+const section = document.getElementsByTagName('section')[0];
+
+const random = (n) => {
+	return Math.floor(Math.random() * n);
+}
+
+for (let i = 0; i < 50; i++) {
+	const div = document.createElement('div');
+	div.setAttribute('style', `
+		top: ${random(350)}px;
+		left: ${random(500)}px;
+	`);
+	section.appendChild(div);
+}
