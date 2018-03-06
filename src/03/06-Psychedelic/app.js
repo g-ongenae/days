42796eebce4e1f13a41fa3034118135485d9848a
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-03-06 23:59:57 +0100

    Add Psychedelic

---
+++ src/03/06-Psychedelic/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.getElementsByTagName('canvas')[0];
+const $ = canvas.getContext('2d');
+
+const circle = () => {
+    // Clear Canvas
+    $.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw circle
+    $.beginPath();
+    $.arc(
+        canvas.width / 2,
+        canvas.height / 2,
+        Math.floor(Math.random() * 75),
+        0,
+        Math.PI * 2
+    );
+    $.stroke();
+
+    requestAnimationFrame(circle);
+};
+
+// Init
+circle();
