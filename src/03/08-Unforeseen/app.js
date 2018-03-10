c59795bd7d22fb5890f64091d2582a29d30f7091
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2018-03-10 20:54:59 +0100

    Lint files

--- src/03/08-Unforeseen/app.js
+++ src/03/08-Unforeseen/app.js
@@ -8,47 +8,47 @@ const side = canvas.height / 3;
 let shift = 0;
 
 const drawRect = (points) => {
-    const last = points.length - 1;
+	const last = points.length - 1;
 
-    $.clearRect(0, 0, canvas.width, canvas.height);
+	$.clearRect(0, 0, canvas.width, canvas.height);
 
-    $.beginPath();
-    $.moveTo(points[last].x, points[last].y);
-    for (const p of points) {
-        $.lineTo(p.x, p.y);
-    }
-    $.fillStyle = '#' + (Math.random() * 0xFFFFFF<<0).toString(16);
-    $.fill();
+	$.beginPath();
+	$.moveTo(points[last].x, points[last].y);
+	for (const p of points) {
+		$.lineTo(p.x, p.y);
+	}
+	$.fillStyle = '#' + (Math.random() * 0xFFFFFF<<0).toString(16);
+	$.fill();
 };
 
-const generateRectPoints = (originPosition, sideLength, shift) => {
-    const points = [];
+const generateRectPoints = (originPosition, sideLength, variation) => {
+	const points = [];
 
-    for (let i = 0; i < 4; i++) {
-        const x = originPosition + (i > 0 && i <= 2 ? sideLength : 0);
-        const y = originPosition + (i >= 2 ? sideLength : 0);
+	for (let i = 0; i < 4; i++) {
+		const x = originPosition + (i > 0 && i <= 2 ? sideLength : 0);
+		const y = originPosition + (i >= 2 ? sideLength : 0);
 
-        points.push({
-            x: i < 2 ? x + shift : x - shift, 
-            y: i < 2 ? y - shift : y + shift,
-        });
-    }
+		points.push({
+			x: i < 2 ? x + variation : x - variation,
+			y: i < 2 ? y - variation : y + variation,
+		});
+	}
 
-    return points;
+	return points;
 };
 
 const generateRect = () => {
-    if (shift > side / 2) {
-        shift -= side / 2;
-        shift = -shift;
-    }
+	if (shift > side / 2) {
+		shift -= side / 2;
+		shift = -shift;
+	}
 
-    const points = generateRectPoints(origin, side, shift);
-    drawRect(points);
+	const points = generateRectPoints(origin, side, shift);
+	drawRect(points);
 
-    shift++;
+	shift++;
 
-    requestAnimationFrame(generateRect);
+	requestAnimationFrame(generateRect);
 };
 
 generateRect();
--- src/03/09-ColorfulCircles/app.js
+++ src/03/09-ColorfulCircles/app.js
@@ -6,22 +6,22 @@ const $ = canvas.getContext('2d');
 const THRESHOLD = (canvas.width < canvas.height ? canvas.width : canvas.height) / 2;
 
 const drawAt = (x, y) => {
-    // $.clearRect(0, 0, canvas.width, canvas.height);
+	// $.clearRect(0, 0, canvas.width, canvas.height);
 
-    $.beginPath();
-    $.arc(
-        x * canvas.width / canvas.clientWidth,
-        y * canvas.height / canvas.clientHeight,
-        Math.floor(Math.random() * THRESHOLD),
-        0,
-        Math.PI * 2
-    );
-    $.fillStyle = '#' + (Math.random() * 0xFFFFFF<<0).toString(16);
-    $.fill();
+	$.beginPath();
+	$.arc(
+		x * canvas.width / canvas.clientWidth,
+		y * canvas.height / canvas.clientHeight,
+		Math.floor(Math.random() * THRESHOLD),
+		0,
+		Math.PI * 2
+	);
+	$.fillStyle = '#' + (Math.random() * 0xFFFFFF<<0).toString(16);
+	$.fill();
 };
 
 drawAt((canvas.clientWidth / 2), (canvas.clientHeight / 2));
 
 canvas.onclick = (event) => {
-    drawAt(event.layerX, event.layerY);
+	drawAt(event.layerX, event.layerY);
 };
